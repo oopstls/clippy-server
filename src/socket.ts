a13ffec0e2c9617bb3b47ecb9ff17a6927802896
oopstls <tlshoaret@gmail.com>
2025-04-11 09:00:44 +0800

    Optimize code

--- src/socket.ts
+++ src/socket.ts
@@ -14,11 +14,9 @@ function handleSocket(io: Server) {
   io.on('connection', (socket: Socket) => {
     generalLog(new Date(), `新客户端连接: ${socket.id}`);
 
-    // 监听注册事件，携带房间 ID 和用户 ID
     socket.on('register', async (registerData: RegisterData) => {
       const { room, userId } = registerData;
 
-      // 非空检查
       if (!room || !userId) {
         generalLog(new Date(), `Socket ${socket.id} 注册失败：房间号或用户ID为空`);
         socket.emit('registrationError', { message: 'Room ID and User ID cannot be empty.' });
@@ -28,7 +26,6 @@ function handleSocket(io: Server) {
 
       generalLog(new Date(), `Socket ${socket.id} 尝试注册房间: ${room}, 用户ID: ${userId}`);
 
-      // 检查是否房间已存在
       if (!roomUsers.has(room)) {
         roomUsers.set(room, new Map());
         generalLog(new Date(), `已创建新的房间: ${room}`);
@@ -36,48 +33,41 @@ function handleSocket(io: Server) {
 
       const usersInRoom = roomUsers.get(room)!;
 
-      // 注册用户
       usersInRoom.set(userId, socket);
       socket.join(room);
       generalLog(new Date(), `Socket ${socket.id} 成功注册房间: ${room}, 用户ID: ${userId}`);
 
-      // 获取历史消息并发送给该用户
       try {
         const historyMessages: Message[] = getMessages(room);
-        // 在发送历史消息前清空消息区域
         socket.emit('history', historyMessages);
-        generalLog(new Date(), `发送历史消息给 Socket ${socket.id}，房间: ${room}`);
+        generalLog(new Date(), `发送历史消息给 Socket ${userId}，房间: ${room}`);
       } catch (error) {
         generalLog(new Date(), `获取历史消息失败 | 房间: ${room} | 错误: ${error}`);
         socket.emit('historyError', { message: 'Failed to retrieve history messages.' });
       }
 
-      // 监听同步数据事件
       socket.on('sendMessage', (data: Message) => {
         messageLog(new Date(), room, userId, data.type);
 
-        // 检查clipReg值是否有效
         let clipReg = data.clipReg;
         if (data.type === 'image') {
-          clipReg = undefined; // 图片类型强制clipReg为空
+          clipReg = undefined;
         } else if (clipReg !== undefined && (clipReg < 0 || clipReg > 5 || !Number.isInteger(clipReg))) {
           generalLog(new Date(), `无效的clipReg值 | 房间: ${room} | 用户ID: ${userId} | clipReg: ${clipReg}`);
           socket.emit('error', { message: 'Invalid clipReg value. It must be an integer between 0 and 5.' });
-          return; // 如果clipReg无效，不处理消息
+          return;
         }
 
-        // 存储消息到数据库，获取服务器记录的时间戳和ID
         let result: {id: number, timestamp: string};
         try {
           result = insertMessage(room, userId, data.type, data.content, clipReg);
         } catch (error) {
           generalLog(new Date(), `存储消息失败 | 房间: ${room} | 用户ID: ${userId} | 类型: ${data.type} | 错误: ${error}`);
-          socket.emit('error', { message: 'Failed to store message.' }); // 发送错误给客户端
-          return; // 如果存储失败，不广播消息
+          socket.emit('error', { message: 'Failed to store message.' });
+          return;
         }
 
-        // 广播消息到房间内的所有用户，包括发送者，附带发送者的 userId、timestamp 和 id
-        const dataToSend: Message = {
+        const dataToBroadcast: Message = {
           id: result.id,
           type: data.type,
           content: data.content,
@@ -85,12 +75,11 @@ function handleSocket(io: Server) {
           timestamp: result.timestamp,
           clipReg: clipReg
         };
-        io.in(room).emit('sendMessage', dataToSend);
+        io.in(room).emit('broadcast', dataToBroadcast);
       });
 
-      // 处理客户端断开连接
       socket.on('disconnect', () => {
-        generalLog(new Date(), `Socket断开连接: ${socket.id}`);
+        generalLog(new Date(), `Socket断开连接: ${userId}`);
         const users = roomUsers.get(room);
         if (users) {
           users.delete(userId);
@@ -98,7 +87,6 @@ function handleSocket(io: Server) {
 
           if (users.size === 0) {
             roomUsers.delete(room);
-            // 关闭房间的数据库连接
             closeRoomDB(room);
             generalLog(new Date(), `房间 ${room} 中的所有用户已断开，数据库连接已关闭`);
           }
